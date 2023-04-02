9962a3e272abdc5f502f68f84b7673a53446438e
1arsalan <124086007+1arsalan@users.noreply.github.com>
2023-04-02 11:31:22 -0700

    Add files via upload
    Polymorphism oops project.py

---
+++ Polymorphism oops project.py
@@ -0,0 +1,18 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+class Cow(Animal):
+    def speak(self):
+        return "Moo!"
